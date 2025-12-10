b4f81d2dca2b98cc031ccd899acbbea49f9797d3
robertasantanatec <robertaxavier24@gmail.com>
2025-12-09 21:52:30 -0300

    Correcao cors

--- src/main.ts
+++ src/main.ts
@@ -3,12 +3,20 @@ import { NestFactory } from '@nestjs/core';
 import express from 'express';
 import { ExpressAdapter } from '@nestjs/platform-express';
 import { AppModule } from './infra/modules/app.module';
+import cors from 'cors';
 
 dotenv.config();
+
 async function bootstrap() {
   const server = express();
+  server.use(
+    cors({
+      origin: ['http://127.0.0.1:5500', 'http://localhost:5500'],
+      credentials: true,
+    }),
+  );
   const app = await NestFactory.create(AppModule, new ExpressAdapter(server));
-  app.enableCors({ origin: '*' });
   await app.listen(3001);
 }
+
 bootstrap();
